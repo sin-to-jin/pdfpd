722aea0c28e3813f5cb4008223ffc1af876b8f8f
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2016-09-12 23:20:26 +0900

    Add pdf sample.

--- app/controllers/PdfController.scala
+++ app/controllers/PdfController.scala
@@ -1,16 +1,25 @@
 package controllers
 
+import com.google.inject.Inject
 import helpers.{ResponseHelper, Success}
+import it.innove.play.pdf.PdfGenerator
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 /**
   * Created by shn on 2016/09/11.
   */
-class PdfController extends Controller with ResponseHelper {
+class PdfController @Inject()(pdfGenerator: PdfGenerator) extends Controller with ResponseHelper {
+
+  val host = "localhost:9000"
+  val contentType = "application/pdf"
 
   def index = Action { implicit rs =>
     Ok(Json prettyPrint responseHTTP(Success))
+  }
+
+  def sample = Action { implicit rs =>
+    Ok(pdfGenerator.toBytes(views.html.pdf.sample.render("sample", "hogehoge"), host)).as(contentType)
   }
 
 }
