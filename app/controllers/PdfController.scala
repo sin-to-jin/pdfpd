373ce10bb3225265311868c51dfee4afe45ec916
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2016-09-13 00:33:08 +0900

    refactoring.

--- app/controllers/PdfController.scala
+++ app/controllers/PdfController.scala
@@ -1,31 +1,26 @@
 package controllers
 
 import com.google.inject.Inject
-import helpers.{PdfHelper, ResponseHelper, Success}
+import helpers.{ResponseHelper, Success}
 import it.innove.play.pdf.PdfGenerator
 import play.Configuration
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
+import helpers.PdfHelper._
 
 /**
   * Created by shn on 2016/09/11.
   */
 class PdfController @Inject()(configuration: Configuration, pdfGenerator: PdfGenerator) extends Controller with ResponseHelper {
-
-  val host = configuration getString PdfHelper.host
-  val contentType = configuration getString PdfHelper.contentType
-  val samplePdf = s"${configuration getString PdfHelper.directoryPdf}sample.pdf"
-
   def index = Action { implicit rs =>
     Ok(Json prettyPrint responseHTTP(Success))
   }
 
   def sample = Action { implicit rs =>
-    Ok(pdfGenerator toBytes(views.html.pdf.sample render("sample", "hogehoge"), host)) as contentType
+    Ok(pdfGenerator toBytes(views.html.pdf.sample render("sample", "hogehoge"), configuration.host)).as(configuration.contentType)
   }
 
   def sampleByInFile = Action { implicit rs =>
-    import helpers.PdfHelper._
-    Ok sendFile(content = samplePdf readCurrentPath, fileName = _ => "sample.pdf", inline = true)
+    Ok sendFile(content = configuration.samplePdf readCurrentPath, fileName = _ => "sample.pdf", inline = true)
   }
 }
--- app/helpers/PdfHelper.scala
+++ app/helpers/PdfHelper.scala
@@ -1,6 +1,7 @@
 package helpers
 
 import java.io.File
+import play.Configuration
 
 /**
   * Created by shn on 2016/09/12.
@@ -9,9 +10,17 @@ case class PdfHelper()
 
 object PdfHelper {
 
-  val host = "pdf.http.local.host"
-  val contentType = "pdf.http.content.type"
-  val directoryPdf = "pdf.https.directory.path"
+  implicit class C(configuration: Configuration) {
+    implicit val HTTPHostAndPort = "pdf.http.local.host"
+    implicit val HTTPContentType = "pdf.http.content.type"
+    implicit val HTTPDirectoryPdf = "pdf.https.directory.path"
+
+    def host: String = configuration getString HTTPHostAndPort
+
+    def contentType: String = configuration getString HTTPContentType
+
+    def samplePdf: String = s"${configuration getString HTTPDirectoryPdf}sample.pdf"
+  }
 
   implicit class P(path: String) {
     def currentPath: String = s"${new File(".").getAbsoluteFile getParent}/$path"
